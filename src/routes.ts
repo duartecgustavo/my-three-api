2bf66a6c8f2e36d80819be0d0fa0533f3c5e834a
ClaudemirRomanoDosSantos-GustavoCastanho-LeilaRibeiro-RenataAntoniassi-ViniciusMello <duartecgustavo@outlook.com>
2023-11-09 18:41:36 -0300

    .

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,9 @@ import { Router, request, response, Request, Response } from "express";
 import { getUsers } from "./controller/UserController";
 const routes = Router();
 
+routes.get("/", (request: Request, response: Response) => {
+  return response.json({ message: "É 13 PORRA!!!" });
+});
 routes.get("/home", (request: Request, response: Response) => {
   return response.json({ message: "É 13 PORRA!!!" });
 });
